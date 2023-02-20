20937b919089a2f0ef9db3a67cca70dcfb8410ef
rpis <rpis9999@gmail.com>
2023-02-20 21:20:28 +0000

    initial version

--- index.js
+++ index.js
@@ -1,22 +1,52 @@
+const { CosmosClient } = require("@azure/cosmos");
 const http = require("http");
 const port = process.env.PORT || 3000
 
-const response = [{
-    "id": 1,
-    "name": "John Doe",
-    "email": "john.doe@wp.pl"
-}, {
-    "id": 2,
-    "name": "Katerina Pavelkova",
-    "email": "k.pav@nevi.cz"
-}];
+let container = null;
+const cosmosClient = new CosmosClient(
+    process.env.COSMOSDB_CONNECTION_STRING,
+);
 
-const requestListener = function (req, res) {
+const requestListener = async function (req, res) {
     console.log("Call service :", req.method, req.url);
     if (req.method === "GET" && req.url === "/") {
+        var users = (await container.items.readAll().fetchAll()).resources
         res.setHeader("Content-Type", "application/json");
         res.writeHead(200);
-        res.end(JSON.stringify(response));
+        res.end(JSON.stringify(users));
+    } else if (req.method === "GET" && req.url.startsWith("/") && req.url.length > 1) {
+        var id = req.url.substring(1);
+        var user = (await container.item(id, id).read()).resource
+        res.setHeader("Content-Type", "application/json");
+        res.writeHead(200);
+        res.end(JSON.stringify(user));
+    } else if (req.method === "POST" && req.url === "/") {
+        var body = '';
+        req.on('data', function (data) {
+            body += data;
+        });
+        req.on('end', async function () {
+            try {
+                var b = JSON.parse(body);
+                var user = {
+                    id: b.id,
+                    partitionKey: b.id,
+                    name: b.name,
+                    email: b.email,
+                    status: "INACTIVE",
+                };
+                var ret = (await container.items.create(user)).resource;
+                res.setHeader("Content-Type", "application/json");
+                res.writeHead(200);
+                res.end(JSON.stringify(ret));
+            } catch (err) {
+                console.log(err);
+                res.setHeader("Content-Type", "application/json");
+                res.writeHead(500);
+                res.end(`{ "message": "Internal server error"}`);
+            }
+        });
+
     } else {
         res.setHeader("Content-Type", "application/json");
         res.writeHead(200);
@@ -25,6 +55,13 @@ const requestListener = function (req, res) {
 };
 
 const server = http.createServer(requestListener);
-server.listen(port, () => {
+server.listen(port, async () => {
+    const { database } = await cosmosClient.databases.createIfNotExists({
+        id: 'users',
+    });
+    container = (await database.containers.createIfNotExists({
+        id: 'users',
+        partitionKey: '/partitionKey',
+    })).container;
     console.log(`Server is running on http://localhost:${port}`);
 });
